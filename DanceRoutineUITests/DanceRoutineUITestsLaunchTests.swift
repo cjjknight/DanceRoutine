50aa95c3733b0de7e7b0c8c8f791d70a867180e7
cjjknight <christopher.johnson42@gmail.com>
2024-08-03 18:28:41 -0500

    Initial Commit

---
+++ DanceRoutineUITests/DanceRoutineUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DanceRoutineUITestsLaunchTests.swift
+//  DanceRoutineUITests
+//
+//  Created by Christopher Johnson on 8/3/24.
+//
+
+import XCTest
+
+final class DanceRoutineUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
